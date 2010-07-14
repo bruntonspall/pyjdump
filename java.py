05fcd55092c4553e559c7b54edec3b17964ed862
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-07-14 18:53:30 +0100

    Clean up, return dictionary

--- java.py
+++ java.py
@@ -1,17 +1,22 @@
 import subprocess
 
+def map_process(process):
+	return {
+	'user': process[0],
+	'pid': process[1],
+	'cmd': process[10],
+	'args': ' '.join(process[11:]),	
+	}
+	
 def get_processlist():
-	return subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).stdout.readlines()
+	return [map_process(l.split()) for l in subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).stdout.readlines()]
 	
 def get_java_processes():
 	ps = get_processlist()
-	return [l.split() for l in ps if l.split()[10].endswith('java')]
+	return [l for l in ps if l['cmd'].endswith('java')]
 	
 def get_pid(process):
-	return process[1]
-		
-def dump_stack(pid, filename):
-	subprocess.Popen(['jmap', '-dump:file=%s' % filename, pid])
+	return process['pid']
 	
 if __name__ == '__main__':
 	print [get_pid(p) for p in get_java_processes()]
